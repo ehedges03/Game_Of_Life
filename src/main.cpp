d88476d38bb7fe0028cb318e8699c3a563c968cb
ehedges <elijahhedges03@gmail.com>
2023-05-24 18:03:10 -0400

    Added required dependencies and got a screen to pop up

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,71 @@
 #include "Utils/WrappedPoint.h"
 #include "GameBoard.h"
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 void simpleBitArrayTest();
 void simpleWrappedPointTest();
 
 int main()
 {
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    /* Create a windows mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "ERROR!\n";
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    uint32_t buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     simpleBitArrayTest();
     simpleWrappedPointTest();
+    return 0;
 }
 
 void simpleBitArrayTest() {
