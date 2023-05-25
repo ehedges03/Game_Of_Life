e75cfd1487199e7628407a7ee4669aa90bdcac86
ehedges <elijahhedges03@gmail.com>
2023-05-24 20:41:40 -0400

    Added debugging to opengl calls, currently only good for vs because of __debugbreak() :/

--- src/main.cpp
+++ src/main.cpp
@@ -7,166 +7,183 @@
 #include <fstream>
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+// TODO: Dev version attaches this, but prod version just has passthrough, not sure how to do this
+#define GLCall(x) GLClearError(); x; ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
 void simpleBitArrayTest();
 void simpleWrappedPointTest();
 
 // Helpful documentaion: https://docs.gl/
 
+static void GLClearError() {
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line) {
+	bool noError = true;
+	while (GLenum error = glGetError()) {
+		std::cout << "[OpenGL Error] (0x" << std::hex << error << std::dec << ") " << function << " " << file << ":" << line << std::endl;
+		noError = false;
+	}
+	return noError;
+}
+
 static std::string readFile(const std::string& filepath) {
-    std::ifstream stream(filepath);
+	std::ifstream stream(filepath);
 
-    std::stringstream sstr;
+	std::stringstream sstr;
 
-    while (stream >> sstr.rdbuf());
+	while (stream >> sstr.rdbuf());
 
-    return sstr.str();
+	return sstr.str();
 }
 
 static uint32_t CompileShader(uint32_t type, const std::string& source) {
-    uint32_t id = glCreateShader(type);
-    const char* src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
-    
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-    if (result == GL_FALSE) {
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = new char[length];
-        glGetShaderInfoLog(id, length, &length, message);
-        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader!" << std::endl;
-        std::cout << message << std::endl;
-    }
+	GLCall(uint32_t id = glCreateShader(type));
+	const char* src = source.c_str();
+	GLCall(glShaderSource(id, 1, &src, nullptr));
+	GLCall(glCompileShader(id));
 
-    return id;
+	int result;
+	GLCall(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
+	if (result == GL_FALSE) {
+		int length;
+		GLCall(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
+		char* message = new char[length];
+		GLCall(glGetShaderInfoLog(id, length, &length, message));
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader!" << std::endl;
+		std::cout << message << std::endl;
+	}
+
+	return id;
 }
 
 static uint32_t CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
-    uint32_t program = glCreateProgram();
-    uint32_t vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-    uint32_t fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+	GLCall(uint32_t program = glCreateProgram());
+	GLCall(uint32_t vs = CompileShader(GL_VERTEX_SHADER, vertexShader));
+	GLCall(uint32_t fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader));
 
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
+	GLCall(glAttachShader(program, vs));
+	GLCall(glAttachShader(program, fs));
+	GLCall(glLinkProgram(program));
+	GLCall(glValidateProgram(program));
 
-    glDeleteShader(vs);
-    glDeleteShader(fs);
+	GLCall(glDeleteShader(vs));
+	GLCall(glDeleteShader(fs));
 
-    return program;
+	return program;
 }
 
 int main()
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit()) {
-        return -1;
-    }
+	/* Initialize the library */
+	if (!glfwInit()) {
+		return -1;
+	}
 
-    /* Create a windows mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        return -1;
-    }
+	/* Create a windows mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-    if (glewInit() != GLEW_OK) {
-        std::cout << "ERROR!\n";
-    }
+	if (glewInit() != GLEW_OK) {
+		std::cout << "ERROR!\n";
+	}
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
+	GLCall(std::cout << glGetString(GL_VERSION) << std::endl);
 
-    float positions[] = {
-        -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.5f,  0.5f,
-        -0.5f,  0.5f,
-    };
+	float positions[] = {
+		-0.5f, -0.5f,
+		 0.5f, -0.5f,
+		 0.5f,  0.5f,
+		-0.5f,  0.5f,
+	};
 
-    uint32_t indices[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
+	uint32_t indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
 
-    uint32_t buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+	uint32_t buffer;
+	GLCall(glGenBuffers(1, &buffer));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+	GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+	GLCall(glEnableVertexAttribArray(0));
+	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
 
-    uint32_t ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(uint32_t), indices, GL_STATIC_DRAW);
+	uint32_t ibo;
+	GLCall(glGenBuffers(1, &ibo));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(uint32_t), indices, GL_STATIC_DRAW));
 
-    std::string vertexShader = readFile("res/shaders/Basic.vertex.glsl");
+	std::string vertexShader = readFile("res/shaders/Basic.vertex.glsl");
 
-    std::string fragmentShader = readFile("res/shaders/Basic.fragment.glsl");
+	std::string fragmentShader = readFile("res/shaders/Basic.fragment.glsl");
 
-    uint32_t shader = CreateShader(vertexShader, fragmentShader);
-    glUseProgram(shader);
+	uint32_t shader = CreateShader(vertexShader, fragmentShader);
+	GLCall(glUseProgram(shader));
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window)) {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window)) {
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
 
-    glDeleteProgram(shader);
+	GLCall(glDeleteProgram(shader));
 
-    glfwTerminate();
+	glfwTerminate();
 
-    simpleBitArrayTest();
-    simpleWrappedPointTest();
-    return 0;
+	simpleBitArrayTest();
+	simpleWrappedPointTest();
+	return 0;
 }
 
 void simpleBitArrayTest() {
-    BitArray mybits = BitArray(9);
+	BitArray mybits = BitArray(9);
 
-    std::cout << mybits << '\n';
+	std::cout << mybits << '\n';
 
-    mybits.set(0, true);
-    mybits.set(2, true);
-    mybits.set(4, true);
-    mybits.set(6, true);
-    mybits.set(8, true);
+	mybits.set(0, true);
+	mybits.set(2, true);
+	mybits.set(4, true);
+	mybits.set(6, true);
+	mybits.set(8, true);
 
-    std::cout << mybits << '\n';
+	std::cout << mybits << '\n';
 
-    mybits.set(0, false);
-    mybits.set(4, false);
-    mybits.set(8, false);
+	mybits.set(0, false);
+	mybits.set(4, false);
+	mybits.set(8, false);
 
-    std::cout << mybits << '\n';
+	std::cout << mybits << '\n';
 }
 
 void simpleWrappedPointTest() {
-    // test WrappedPoint -- imagine all the unit tests yay
+	// test WrappedPoint -- imagine all the unit tests yay
 
-    WrappedPoint testWPoint1({ -2, -6 }, { 5, 5 });
-    WrappedPoint testWPoint2({ 5, 15 }, { 5, 5 });
+	WrappedPoint testWPoint1({ -2, -6 }, { 5, 5 });
+	WrappedPoint testWPoint2({ 5, 15 }, { 5, 5 });
 
-    bool result1 = (testWPoint1.x() == 3) && (testWPoint1.y() == 4);
-    bool result2 = (testWPoint2.x() == 0) && (testWPoint2.y() == 0);
+	bool result1 = (testWPoint1.x() == 3) && (testWPoint1.y() == 4);
+	bool result2 = (testWPoint2.x() == 0) && (testWPoint2.y() == 0);
 
-    std::cout << '\n' << "negative wrap: " << (result1 ? "success " : "failed ") << testWPoint1.x() << ", " << testWPoint1.y();
-    std::cout << '\n' << "positive wrap: " << (result2 ? "success " : "failed ") << testWPoint2.x() << ", " << testWPoint2.y();
+	std::cout << '\n' << "negative wrap: " << (result1 ? "success " : "failed ") << testWPoint1.x() << ", " << testWPoint1.y();
+	std::cout << '\n' << "positive wrap: " << (result2 ? "success " : "failed ") << testWPoint2.x() << ", " << testWPoint2.y();
 }
