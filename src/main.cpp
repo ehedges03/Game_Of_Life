027fadba3e59180074e660f25af0d4d21e46f16e
Eli Hedges <ehedges03@proton.me>
2025-05-02 18:40:38 -0400

    Got window opening

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,15 @@
 #include "LibFunni/log.h"
 #include "Utils/Console.h"
 #include "Utils/WrappedPoint.h"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
 void simpleBitArrayTest();
 void simpleChunkTest();
 void simpleGameBoardTest();
 void simpleWrappedPointTest();
 void simpleLoggerTest();
+void simpleGLFWWindow();
 
 int main() {
   simpleBitArrayTest();
@@ -21,6 +24,7 @@ int main() {
   // simpleChunkTest();
   // simpleGameBoardTest();
   simpleLoggerTest();
+  simpleGLFWWindow();
 }
 
 void simpleBitArrayTest() {
@@ -148,4 +152,52 @@ void simpleLoggerTest() {
   logger.logi();
   logger.logw();
   logger.logi();
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+void simpleGLFWWindow() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Game Of Life", nullptr, nullptr);
+
+  if (window == nullptr) {
+    const char* description;
+    glfwGetError(&description);
+    std::cout << "Failed to create window Error: " << description << std::endl;   
+    glfwTerminate();
+    return;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return;
+  }
+
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while(!glfwWindowShouldClose(window))
+  {
+      processInput(window); 
+
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
 }
